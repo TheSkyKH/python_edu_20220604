ceb972c47b6c1060a61ffdb0566f8056febe9c28
TheSkyKH <108981308+TheSkyKH@users.noreply.github.com>
2022-07-09 20:19:12 +0900

    initial
    강사님 실습내용

---
+++ lotto.py
@@ -0,0 +1,20 @@
+﻿import random
+lotto = []
+qty = int(input("구매 수량을 입력해주세요"))
+cnt = 1
+while cnt <= qty:
+    i = 1
+    while i <= 45:
+        lotto.append(i)
+        i += 1
+    lottosize = len(lotto) # 45
+    i = 1
+    while i <= 6:
+        lottosize -= 1
+        idx = random.randint(0, lottosize)
+        lottonum = lotto.pop(idx)
+        print(lottonum, end=", ")
+        i += 1
+    lotto.clear()
+    cnt += 1
+    print()
---
+++ mo1.py
@@ -0,0 +1,28 @@
+﻿class FourCal21:
+    def __init__(self, num1, num2): 
+        self.first = num1           
+        self.second = num2
+    def add(self):
+        self.result = self.first + self.second
+    def div(self):
+        self.result = self.first / self.second
+
+class MoreFourCal11(FourCal21): # FourCal2를 상속 받음
+    def mul(self):
+        self.result = self.first * self.second
+    def sub(self):
+        self.result = self.first - self.second
+    def div(self): # 상속 받은 메서드를 다시 정의 하는 것 : 오버라이딩
+        if self.second == 0:
+            self.result = 0
+        else:
+            self.result = self.first / self.second  
+
+PI = 3.141592
+
+def sum1(num1, num2):
+    result = num1 + num2
+    return result
+
+
+dddd = MoreFourCal11(100, 20)
